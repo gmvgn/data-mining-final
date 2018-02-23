eaefff30d93e348ae5c96024c20011d6e68de665
Garrett Glissmann <captain@econ1-162-95-dhcp.int.colorado.edu>
2018-02-23 15:03:42 -0700

    Part 1 pdf

--- project.py
+++ project.py
@@ -5,14 +5,17 @@ import pandas as pd
 import matplotlib as plt
 import scipy
 
-def get_data_frame(file_name):
-    df = pd.read_csv(file_name)
+def get_data_frame(file_name, nrows=None):
+    df = pd.read_csv(file_name, nrows=nrows)
     # Data formatting
     return df
 
 def main(file_name):
-    df = get_data_frame(file_name)
-    print(df.head())
+    df_working = get_data_frame(file_name, 100000)
+    df_working['Date'] = pd.to_datetime(df_working['Date'])
+    filtered = df_working[df_working['Primary Type'] == 'HOMICIDE']
+    test = filtered.groupby([(filtered.Date.dt.year), (filtered.Date.dt.month), 'Community Area']).agg(['count'])
+    print(test.head())
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Data Mining Project')
